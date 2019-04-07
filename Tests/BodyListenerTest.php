0fbc5e615d5239e5daa7a366bfe02faea25a6649
tarlepp <tarmo.leppanen@protacon.com>
2019-04-07 12:49:11 +0300

    tests: Implemented more tests for `BodyListener`

--- Tests/BodyListenerTest.php
+++ Tests/BodyListenerTest.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types = 1);
 
 namespace Protacon\Bundle\RestListenersBundle\Tests;
 
@@ -8,6 +9,7 @@ use ReflectionException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 use Symfony\Component\HttpKernel\HttpKernelInterface;
+use function sprintf;
 
 class BodyListenerTest extends TestCase
 {
@@ -19,11 +21,57 @@ class BodyListenerTest extends TestCase
         /** @var HttpKernelInterface $httpKernel */
         $httpKernel = $this->getMockBuilder(HttpKernelInterface::class)->disableOriginalConstructor()->getMock();
         $request = new Request();
-        $event = new GetResponseEvent($httpKernel, $request, HttpKernelInterface::MASTER_REQUEST);
+        $event = new GetResponseEvent($httpKernel, clone $request, HttpKernelInterface::MASTER_REQUEST);
 
         $listener = new BodyListener();
         $listener->onKernelRequest($event);
 
-        $this->assertSame($request, $event->getRequest());
+        self::assertEquals($request, $event->getRequest());
+    }
+
+    /**
+     * @dataProvider dataProviderTestThatListenerDoesNotDoAnythingIfRequestIsNotJson
+     *
+     * @param string $contentType
+     * @param bool   $changed
+     *
+     * @throws ReflectionException
+     */
+    public function testThatListenerChangesRequestIfNeeded(?string $contentType, bool $changed): void
+    {
+        $content = '{"foo":"bar"}';
+
+        /** @var HttpKernelInterface $httpKernel */
+        $httpKernel = $this->getMockBuilder(HttpKernelInterface::class)->disableOriginalConstructor()->getMock();
+        $request = new Request([], [], [], [], [], ['bar' => 'foo'], $content);
+        $request->headers->set('Content-Type', $contentType);
+        $event = new GetResponseEvent($httpKernel, clone $request, HttpKernelInterface::MASTER_REQUEST);
+
+        $listener = new BodyListener();
+        $listener->onKernelRequest($event);
+
+        $message = sprintf(
+            'Failed with content-type: %s - Request content type: %s',
+            $contentType,
+            $request->getContentType()
+        );
+
+        if ($changed) {
+            self::assertNotEquals($request, $event->getRequest(), $message);
+        } else {
+            self::assertEquals($request, $event->getRequest(), $message);
+        }
+    }
+
+    public function dataProviderTestThatListenerDoesNotDoAnythingIfRequestIsNotJson(): array
+    {
+        return [
+            [null, true],
+            ['text/javascript', false],
+            ['application/json', true],
+            ['text/javascript', false],
+            ['text/txt', true],
+            ['text/html', false],
+        ];
     }
 }
